a2996b2c59949d3b11a206c28e09d750bc9be4a1
ade <a>
2017-02-14 15:33:51 +0100

    initial save

---
+++ Entities.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 09:30:03 2015
+
+@author: ade
+"""
+
+
+def createObj(item):
+    return {"LINE": Line}.get(item[0][1], Entities)(*item)
+
+
+class Entities(object):
+    gcodes_common = [-1, 0, 5, 102, 330, 360, 100, 67, 410, 8, 6, 62, 48, 60, 92, 310]
+    codedict = {}
+
+    def __init__(self, *codelist):
+        self.codedict = {}
+        for key, val in codelist:
+            self.codedict[key] = val
+        self.setDefaults()
+
+    def setDefaults(self):
+        if not self.codedict.has_key('67'):
+            self.codedict['67'] = 0
+        if not self.codedict.has_key('6'):
+            self.codedict['6'] = 'BYLAYER'
+        if not self.codedict.has_key('62'):
+            self.codedict['62'] = 'BYLAYER'
+        if not self.codedict.has_key('48'):
+            self.codedict['48'] = '1.0'
+        if not self.codedict.has_key('60'):
+            self.codedict['60'] = '0'
+
+    def __str__(self):
+        out = ''
+        for key, val in self.codedict.items():
+            out += str(key) + ' : ' + val + '\n'
+        return out
+
+
+class Line(Entities):
+    startpoint = ()
+    endpoint = ()
+    name = 'LINE'
+    gcodes = [100, 39, 10, 20, 30, 11, 21, 31, 210, 220, 230]
+
+    def __init__(self, *codelist):
+        super(Line, self).__init__(*codelist)
+        if self.codedict == {}: pass
+        codes = self.gcodes + self.gcodes_common
+        for key in self.codedict.keys():
+            if int(key) not in codes:
+                raise ValueError('%s group code not accepted' % key)
+        self.startpoint = (self.codedict['10'], self.codedict['20'], self.codedict['30'])
+        self.endpoint = (self.codedict['11'], self.codedict['21'], self.codedict['31'])
+
+    def __str__(self):
+        return str(self.startpoint) + str(self.endpoint)
+
+
+if __name__ == '__main__':
+    print("Entities as main")
+
+    #   a = Entities()
+    b = Line()
---
+++ essai.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 23 nov. 2015
+
+@author: ade
+"""
+
+from Entities import *
+
+
+class Dxf:
+    section = {'HEADER': [],
+               'CLASSES': [],
+               'TABLES': [],
+               'BLOCKS': [],
+               'ENTITIES': [],
+               'OBJECTS': [],
+               'THUMBNAILIMAGE': []
+               }
+
+    def __init__(self, dxffile):
+
+        if dxffile == '': pass
+        self.readfile(dxffile)
+        self.entlist = self.readsection(*self.section['ENTITIES'])
+        self.entity = []
+        for item in self.entlist:
+            self.entity.append(createObj(item))
+
+    def readcode(self, f):
+        """
+        Reads two lines in dxf file, outputs (groupcode, value)
+        """
+        code = f.readline().strip('\n').strip()
+        value = f.readline().strip('\n').strip()
+        return (code, value)
+
+    def readfile(self, dxffile):
+        with open(dxffile, 'r') as f:
+            out = self.readcode(f)
+            while out[1] != 'EOF':
+                if out == ('0', 'SECTION'):
+                    out = self.readcode(f)
+                    sect = out[1]
+                    print
+                    sect
+                    out = self.readcode(f)
+                    while out[1] != "ENDSEC":
+                        self.section[sect] += [out]
+                        out = self.readcode(f)
+                else:
+                    out = self.readcode(f)
+
+    def readsection(self, *seclist):
+        itemlist = []
+        returnlist = []
+        for item in seclist:
+            if item[0] == '0':
+                if itemlist != []:
+                    returnlist.append(itemlist)
+                itemlist = [item]
+            else:
+                itemlist.append(item)
+        returnlist.append(itemlist)
+        return returnlist
+
+
+class Section:
+    None
+    """
+    def __init__(self, *codelist):
+        l = []
+        for codes in codelist:
+            if codes[0] == '0':
+                if l != []: self.entlist.append(l)
+                l = [codes]
+            else: 
+                l.append(codes)
+    """
+
+
+class Header(Section):
+    None
+
+
+class Classes(Section):
+    None
+
+
+class Tables(Section):
+    None
+
+
+class Blocks(Section):
+    None
+
+
+class Objects(Section):
+    None
+
+
+class Thumbnailimage(Section):
+    None
+
+
+a = Dxf('Piece2.DXF')
